1d23ccf0c55abf66f0684a2debbe3bcc9ee8a352
garciamillan <47037116+garciamillan@users.noreply.github.com>
2022-03-30 23:29:52 +0100

    Add files via upload

---
+++ rgm_Gfit.h
@@ -0,0 +1,1 @@
+void rgm_gsl_Gauss_fit (double *t, double *y, double *weights, const size_t n, double *est_A, double *est_mu, double *est_sigmasq, double *std_A, double *std_mu, double *std_sigmasq);
